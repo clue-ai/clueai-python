fb89ed3a7d175e01a3734dd6c63f8f89adddbef5
matrix <zhangxw0413@gmail.com>
2022-09-27 10:01:24 +0800

    update

--- tests/utils.py
+++ tests/utils.py
@@ -2,6 +2,6 @@ import os
 
 
 def get_api_key() -> str:
-    api_key = os.getenv('MO_API_KEY')
+    api_key = os.getenv('CLUAI_API_KEY')
     assert api_key, 'CLUEAI_API_KEY environment variable not set'
     return api_key

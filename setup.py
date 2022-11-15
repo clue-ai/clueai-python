c55844da36802405cfa898e46e8c76c4f20a8293
matrix <zhangxw0413@gmail.com>
2022-11-15 14:33:40 +0800

    update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class BinaryDistribution(Distribution):
 
 setuptools.setup(
     name='clueai',
-    version='0.0.1.6',
+    version='0.0.1.7',
     author='matrix',
     author_email='brightmart@hotmail.com',
     description='A Python library for the ClueAI API',
